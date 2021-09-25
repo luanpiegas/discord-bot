f45fe23d2903fa29ebbbf3c7e2d9e7decbe36737
Luan <luan@luan.des.br>
2021-09-25 03:27:39 -0300

    fix response error

--- pages/api/index.js
+++ pages/api/index.js
@@ -5,12 +5,32 @@ const prefix = "-";
 
 export default (req, res) => {
 
+    client.on('ready', () => {
+        console.log(`Logado como: ${client.user.tag}`)
+    })
+
     client.on("message", function (msg) {
-        console.log(msg)
+        if (msg.author.bot) return;
+        if (!msg.content.startsWith(prefix)) return;
+
+        const commandBody = msg.content.slice(prefix.length);
+        const args = commandBody.split(' ');
+        const command = args.shift().toLowerCase();
+
+        if (command === "ping") {
+            const timeTaken = Date.now() - msg.createdTimestamp;
+            msg.reply(`Pong! Latência: ${timeTaken}ms.`);
+        }
+
+        else if (command === "sum") {
+            const numArgs = args.map(x => parseFloat(x));
+            const sum = numArgs.reduce((counter, x) => counter += x);
+            msg.reply(`A soma é: ${sum}!`);
+        }
     });
     
+    res.status(200).json({})
+    
     client.login(process.env.DISCORD_TOKEN);
-
-    res.status(200).json({ok:true})
 
 }
