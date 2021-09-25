03f2109eed12c01ef4d0e5c523434ef079e02aa5
Luan <luan@luan.des.br>
2021-09-25 03:15:58 -0300

    change lib version

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,4 @@
 const { Client, Intents } = require('discord.js');
-const config = require('../../config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES] });
 const prefix = "-";
 
