1fb92e79fcbf456e7a30c8a5a397112b5fe07b86
Luan <luanpiegas@gmail.com>
2022-04-01 13:22:31 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,28 +4,31 @@ const client = new Discord.Client();
 const prefix = process.env.BOT_PREFIX;
 
 client.on('ready', () => {
-    console.log(`Logado como: ${client.user.tag}`)
+    console.log(`Logged in as: ${client.user.tag}`);
 })
 
+// Check for a new message sent
 client.on("message", function (msg) {
-    if (msg.author.bot) return;
-    if (!msg.content.startsWith(prefix)) return;
+    if (msg.author.bot) return; // if the message was sent by the bot, ignore it.
+    if (!msg.content.startsWith(prefix)) return; // if the message doens't starts with the default prefix, ignore it.
 
+    // Parse the message string
     const commandBody = msg.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-    // Ping
-    if (command === "ping") {
-        const timeTaken = Date.now() - msg.createdTimestamp;
-        msg.reply(`Pong! Latency: ${timeTaken}ms.`);
-    }
-
-    // Sum of two values
-    else if (command === "sum") {
-        const numArgs = args.map(x => parseFloat(x));
-        const sum = numArgs.reduce((counter, x) => counter += x);
-        msg.reply(`A soma é: ${sum}!`);
+    // Commands 
+    switch (command) {
+        case 'ping':
+            const timeTaken = Date.now() - msg.createdTimestamp;
+            msg.reply(`Pong! Latency: ${timeTaken}ms.`);
+        break;
+      case 'sum':
+            const numArgs = args.map(x => parseFloat(x));
+            const sum = numArgs.reduce((counter, x) => counter += x);
+            msg.reply(`A soma é: ${sum}!`);
+      default:
+        return;
     }
 });
 
